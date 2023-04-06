463629b4cb06bad6be70a19cf3b20e42bf8ccc52
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2023-04-06 02:37:12 -0300

    array no js

---
+++ script.js
@@ -0,0 +1,11 @@
+function carregaDicionario() {
+    var biografias = [
+        "Marielle Franco",
+        "Rosa Parks",
+        "Angela Davis",
+        "Carolina de Jesus",
+    ];
+    alert(biografias);
+}
+
+carregaDicionario();
