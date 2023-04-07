5558ad5ab91f340c3f822fd9053d20b266bafa4f
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2023-04-07 03:14:47 -0300

    melhorando o código js

--- script.js
+++ script.js
@@ -39,27 +39,24 @@ function carregaDicionario() {
     };
 
     var content = document.getElementById("content");
-    
-    for (var bio in biografias) {
-        content.innerHTML +=
-        '<div class="card">' +
-        '<img src="' +
-        biografias[bio].imagem +
-        '"/>' +
-        "<details>" +
-        "<summary>" +
-        biografias[bio].nome +
-        "</summary>" +
-        "<p>" +
-        biografias[bio].descricao +
-        "</p>" +
-        "<blockquote><q>" +
-        biografias[bio].citacao +
-        "</q></blockquote>" +
-        "</details></div>";
-    }
-
-console.log(biografias);
+        for (var bio in biografias) {
+            content.innerHTML +=
+            '<div class="card">' +
+            '<img src="' +
+            biografias[bio].imagem +
+            '"/>' +
+            "<details>" +
+            "<summary>" +
+            biografias[bio].nome +
+            "</summary>" +
+            "<p>" +
+            biografias[bio].descricao +
+            "</p>" +
+            "<blockquote><q>" +
+            biografias[bio].citacao +
+            "</q></blockquote>" +
+            "</details></div>";
+        }
 }
 
 carregaDicionario();
